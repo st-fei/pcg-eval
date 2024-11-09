c358357bb23bba72573c05f5ef9fd74f15bd41d7
Tong Fei <134366838+st-fei@users.noreply.github.com>
2024-11-09 04:19:39 +0000

    path-done

--- config.py
+++ config.py
@@ -7,20 +7,20 @@ def get_cfg():
     # File Path
     cfg["FILE_PATH"] = {
         'processed_file':'./data/dataset/ui_sample.json',
-        'raw_test_file':'/workspaces/pcg-eval/data/dataset/test.json',    
-        'sample_file': '/workspaces/pcg-eval/data/dataset/test_sample.txt',
+        'raw_test_file':'./data/dataset/test.json',    
+        'sample_file': './data/dataset/test_sample.txt',
         'rank_file':'',
         'img_dir':'',
-        'sample_img_dir': '/workspaces/pcg-eval/data/pcg_img_sample',
+        'sample_img_dir': './data/pcg_img_sample',
         'navigation_path':'',
         'out_path':''
     }
     # Gen Path
     cfg["GEN_PATH"] = {
-        'ofa_ft_gen_path#1': '/workspaces/pcg-eval/data/generation/ofa.txt',
-        'maria_ft_gen_path#1':'/workspaces/pcg-eval/data/generation/maria.txt',
-        'livebot_ft_gen_path#1':'/workspaces/pcg-eval/data/generation/livebot.txt',
-        'mplug-video_z-shot_gen_path#1':'/workspaces/pcg-eval/data/generation/mplug-video.txt',
+        'ofa_ft_gen_path#1': './data/generation/ofa.txt',
+        'maria_ft_gen_path#1':'./data/generation/maria.txt',
+        'livebot_ft_gen_path#1':'./data/generation/livebot.txt',
+        'mplug-video_z-shot_gen_path#1':'./data/generation/mplug-video.txt',
         'mplug-owl3_z-shot_gen_path#1':'',
         'qwen2-vl_z-shot_gen_path#1':'',
         'minicpm-v_z-shot_gen_path#1':'',
